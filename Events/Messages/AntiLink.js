4854c3d2cfd59f413759feb9ca6d9ab2c6f8ac65
Technology Power <100185728+Bhargav230m@users.noreply.github.com>
2023-08-17 20:43:54 +0530

    Fixed Bugs

--- Events/Messages/AntiLink.js
+++ Events/Messages/AntiLink.js
@@ -19,9 +19,12 @@ module.exports = {
  */
   async execute(message, client) {
     if (!message.guild) return;
+    console.log("Hi");
     if (message.author.bot) return;
+    console.log("Hi");
     if (message.member.permissions.has(PermissionFlagsBits.ManageMessages))
       return;
+    console.log("Hi");
     const guild = message.guild;
     let requireDB = await automod.findOne({ Guild: guild.id });
     let UserData = await UserAM.findOne({
@@ -30,7 +33,9 @@ module.exports = {
     });
 
     if (!requireDB) return;
-    if (requireDB.AntiLink == false) return;
+    console.log("Hi");
+    if (!requireDB.AntiLink) return;
+    console.log("Hi");
 
     const ragex =
       /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
@@ -48,174 +53,180 @@ module.exports = {
     }
 
     if (ragex.test(message.content)) {
+      console.log(ragex.test(message.content));
+
       if (
-        message.content == requireDB.AllowedLinks1 ||
+        message.content !== requireDB.AllowedLinks1 ||
         requireDB.AllowedLinks2 ||
         requireDB.AllowedLinks3 ||
         requireDB.AllowedLinks4
-      )
-        return;
-      try {
-        await message.delete();
-      } catch (err) {
-        return;
-      }
+      ) {
+        try {
+          await message.delete();
+        } catch (err) {
+          return;
+        }
 
-      if (!UserData) {
-        const newData = new UserAM({
-          Guild: guild.id,
-          User: message.author.id,
-          InfractionPoints: 1,
-        });
-        newData.save();
-      } else {
-        console.log(UserData.InfractionPoints);
-        switch (UserData.InfractionPoints) {
-          case 3:
-            if (!message.member) return;
-            const time = ms(requireDB.Timeout);
-            await message.member.timeout(time);
-            logChannel.send({
-              embeds: [
-                new EmbedBuilder()
-                  .setColor("Red")
-                  .setDescription(
-                    `<@${message.author.id}> has been timed out for sending links\n`
-                  ),
-              ],
-            });
-            break;
-          case 8:
-            if (!message.member) return;
-            logChannel.send({
-              embeds: [
-                new EmbedBuilder()
-                  .setColor("Red")
-                  .setDescription(
-                    `<@${message.author.id}> has been kicked for sending links`
-                  ),
-              ],
-            });
-            message.member.send({
-              embeds: [
-                new EmbedBuilder()
-                  .setColor("Red")
-                  .setDescription(
-                    `You have been kicked from ${message.guild.name} for sending links`
-                  ),
-              ],
-            });
-            await message.member
-              .kick({ reason: "Sending Links" })
-              .then(async () => {
-                await UserAM.findOneAndDelete({
-                  Guild: guild,
-                  User: message.author.id,
+        if (!UserData) {
+          const newData = new UserAM({
+            Guild: guild.id,
+            User: message.author.id,
+            InfractionPoints: 1,
+          });
+          newData.save();
+        } else {
+          console.log(UserData.InfractionPoints);
+          switch (UserData.InfractionPoints) {
+            case 3:
+              if (!message.member) return;
+              const time = ms(requireDB.Timeout);
+              await message.member.timeout(time);
+              logChannel.send({
+                embeds: [
+                  new EmbedBuilder()
+                    .setColor("Red")
+                    .setDescription(
+                      `<@${message.author.id}> has been timed out for sending links\n`
+                    ),
+                ],
+              });
+              break;
+            case 8:
+              if (!message.member) return;
+              logChannel.send({
+                embeds: [
+                  new EmbedBuilder()
+                    .setColor("Red")
+                    .setDescription(
+                      `<@${message.author.id}> has been kicked for sending links`
+                    ),
+                ],
+              });
+              message.member.send({
+                embeds: [
+                  new EmbedBuilder()
+                    .setColor("Red")
+                    .setDescription(
+                      `You have been kicked from ${message.guild.name} for sending links`
+                    ),
+                ],
+              });
+              await message.member
+                .kick({ reason: "Sending Links" })
+                .then(async () => {
+                  await UserAM.findOneAndDelete({
+                    Guild: guild,
+                    User: message.author.id,
+                  });
                 });
-              });
-            break;
+              break;
+          }
+          UserData.InfractionPoints += 1;
+          UserData.save();
         }
-        UserData.InfractionPoints += 1;
-        UserData.save();
+
+        const msg = await message.channel.send({ embeds: [embed] });
+        setTimeout(async () => {
+          await msg.delete();
+        }, 5000);
+        const buttons = new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setLabel("Timeout")
+            .setEmoji("⚒️")
+            .setCustomId("timeout")
+            .setStyle(ButtonStyle.Secondary),
+          new ButtonBuilder()
+            .setLabel("Kick")
+            .setEmoji("🔨")
+            .setCustomId("kick")
+            .setStyle(ButtonStyle.Danger)
+        );
+
+        const text = await logChannel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setColor("Red")
+              .setDescription(
+                `<@${message.author.id}> has sent a link.\n\`\`\`${message.content}\`\`\``
+              ),
+          ],
+          components: [buttons],
+        });
+
+        const col = await text.createMessageComponentCollector();
+        col.on("collect", async (m) => {
+          const ms = require("ms");
+          switch (m.customId) {
+            case "timeout":
+              if (
+                !m.member.permissions.has(PermissionFlagsBits.ModerateMembers)
+              )
+                return m.reply({
+                  content: "You don't have permission to timeout",
+                  ephemeral: true,
+                });
+
+              const embed = new EmbedBuilder()
+                .setTitle("Timeout")
+                .setDescription(
+                  `You have received a timeout from \`${message.guild.name}\` for sending links`
+                )
+                .setColor("Red");
+
+              if (!message.member) {
+                return m.reply({
+                  content: "This user doesn't exist",
+                  ephemeral: true,
+                });
+              }
+              m.reply({
+                content: `Timed out ${message.author.tag}`,
+                ephemeral: true,
+              });
+              message.member
+                .send({
+                  embeds: [embed],
+                })
+                .then(() => {
+                  const time = ms("10m");
+                  message.member.timeout(time);
+                });
+              break;
+            case "kick":
+              if (!m.member.permissions.has(PermissionFlagsBits.KickMembers))
+                return m.reply({
+                  content: "You don't have permission to kick",
+                  ephemeral: true,
+                });
+              const embedss = new EmbedBuilder()
+                .setTitle("Kicked")
+                .setDescription(
+                  `You have been kicked from \`${message.guild.name}\` for sending links`
+                )
+                .setColor("Red");
+              if (!message.member) {
+                return m.reply({
+                  content: "This user doesn't exist",
+                  ephemeral: true,
+                });
+              }
+              m.reply({
+                content: `Kicked ${message.author.tag}`,
+                ephemeral: true,
+              });
+              message.member
+                .send({
+                  embeds: [embedss],
+                })
+                .then(() => {
+                  message.member.kick({ reason: "Sending links" });
+                });
+              break;
+          }
+        });
       }
-
-      const msg = await message.channel.send({ embeds: [embed] });
-      setTimeout(async () => {
-        await msg.delete();
-      }, 5000);
-      const buttons = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setLabel("Timeout")
-          .setEmoji("⚒️")
-          .setCustomId("timeout")
-          .setStyle(ButtonStyle.Secondary),
-        new ButtonBuilder()
-          .setLabel("Kick")
-          .setEmoji("🔨")
-          .setCustomId("kick")
-          .setStyle(ButtonStyle.Danger)
-      );
-
-      const text = await logChannel.send({
-        embeds: [
-          new EmbedBuilder()
-            .setColor("Red")
-            .setDescription(
-              `<@${message.author.id}> has sent a link.\n\`\`\`${message.content}\`\`\``
-            ),
-        ],
-        components: [buttons],
-      });
-
-      const col = await text.createMessageComponentCollector();
-      col.on("collect", async (m) => {
-        const ms = require("ms");
-        switch (m.customId) {
-          case "timeout":
-            if (!m.member.permissions.has(PermissionFlagsBits.ModerateMembers))
-              return m.reply({
-                content: "You don't have permission to timeout",
-                ephemeral: true,
-              });
-
-            const embed = new EmbedBuilder()
-              .setTitle("Timeout")
-              .setDescription(
-                `You have received a timeout from \`${message.guild.name}\` for sending links`
-              )
-              .setColor("Red");
-
-            if (!message.member) {
-              return m.reply({
-                content: "This user doesn't exist",
-                ephemeral: true,
-              });
-            }
-            m.reply({
-              content: `Timed out ${message.author.tag}`,
-              ephemeral: true,
-            });
-            message.member
-              .send({
-                embeds: [embed],
-              })
-              .then(() => {
-                const time = ms("10m");
-                message.member.timeout(time);
-              });
-            break;
-          case "kick":
-            if (!m.member.permissions.has(PermissionFlagsBits.KickMembers))
-              return m.reply({
-                content: "You don't have permission to kick",
-                ephemeral: true,
-              });
-            const embedss = new EmbedBuilder()
-              .setTitle("Kicked")
-              .setDescription(
-                `You have been kicked from \`${message.guild.name}\` for sending links`
-              )
-              .setColor("Red");
-            if (!message.member) {
-              return m.reply({
-                content: "This user doesn't exist",
-                ephemeral: true,
-              });
-            }
-            m.reply({
-              content: `Kicked ${message.author.tag}`,
-              ephemeral: true,
-            });
-            message.member
-              .send({
-                embeds: [embedss],
-              })
-              .then(() => {
-                message.member.kick({ reason: "Sending links" });
-              });
-            break;
-        }
-      });
+    } else {
+      return;
     }
   },
 };
